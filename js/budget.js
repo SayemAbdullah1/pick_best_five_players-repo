b0728f719a382bdc2297b14cde42ff308a18b248
SayemAbdullah1 <dxsayem0@gmail.com>
2022-08-22 01:07:01 +0600

    Show total output

--- js/budget.js
+++ js/budget.js
@@ -2,3 +2,15 @@ document.getElementById('calculate').addEventListener('click', function(){
     const PlayerBudgetInput = getInputFieldValueById('per-player');
     setTextElementById('player-expense', PlayerBudgetInput);
 })
+
+
+
+document.getElementById('calculate-total').addEventListener('click', function(){
+    const perplayerBudget = getTextElementById('player-expense');
+    const managerBudget = getInputFieldValueById('manager-input');
+    const couchBudget = getInputFieldValueById('couch-input');
+
+    const totaloutputBudget = perplayerBudget + managerBudget + couchBudget;
+    
+    setTextElementById('total-output', totaloutputBudget);
+})
